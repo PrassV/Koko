d9ac928e69d0b6a782c59c3ba17faa274e381059
PrassV <prassannavenkat@gmail.com>
2025-12-27 20:52:43 +0530

    commit

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 from .users import User, UserRole
 from .properties import Property, Unit
 from .tenancy import Tenancy
-from .maintenance import MaintenanceRequest, RequestStatus
+from .maintenance import MaintenanceRequest, RequestStatus, MaintenanceComment
 from .finance import Payment, PaymentType
